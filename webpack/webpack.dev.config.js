462c1c33540b9b9e59d87051a6e0d00f233aa5e3
f-stone <dreamersfly@163.com>
2021-12-01 19:10:46 +0800

    update(webpack-config): 添加 devtool 配置

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -2,7 +2,7 @@
  * @Description: webpack 开发配置文件
  * @Author: F-Stone
  * @Date: 2021-11-30 18:39:28
- * @LastEditTime: 2021-12-01 14:09:36
+ * @LastEditTime: 2021-12-01 19:07:49
  * @LastEditors: F-Stone
  */
 const { merge } = require("webpack-merge");
@@ -10,6 +10,8 @@ const PUB_CONFIG = require("./webpack.pub.config");
 
 module.exports = (env, argv) => {
     return merge(PUB_CONFIG, {
+        target: "web",
         mode: "development",
+        devtool: "eval-source-map",
     });
 };
--- webpack/webpack.pro.config.js
+++ webpack/webpack.pro.config.js
@@ -2,7 +2,7 @@
  * @Description: webpack 生产配置文件
  * @Author: F-Stone
  * @Date: 2021-11-30 18:39:40
- * @LastEditTime: 2021-12-01 15:35:40
+ * @LastEditTime: 2021-12-01 19:09:01
  * @LastEditors: F-Stone
  */
 const { merge } = require("webpack-merge");
@@ -12,6 +12,7 @@ const PUB_CONFIG = require("./webpack.pub.config");
 module.exports = (env, argv) => {
     return merge(PUB_CONFIG, {
         mode: "production",
+        devtool: false,
         plugins: WEBPACK_PRO_PLUGINS
     });
 };
