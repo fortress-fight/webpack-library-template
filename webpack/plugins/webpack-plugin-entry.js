3ff8ee607feac817cc254cd16bd01600dbb5be45
f-stone <dreamersfly@163.com>
2021-12-01 15:40:08 +0800

    feature(webpack-plugin): 添加 clean-webpack-plugin

--- webpack/plugins/webpack-plugin-entry.js
+++ webpack/plugins/webpack-plugin-entry.js
@@ -2,12 +2,16 @@
  * @Description: webpack-plugin 的入口文件
  * @Author: F-Stone
  * @Date: 2021-12-01 14:50:48
- * @LastEditTime: 2021-12-01 15:03:34
+ * @LastEditTime: 2021-12-01 15:35:15
  * @LastEditors: F-Stone
  */
 
 const { HTML_PLUGINS } = require("./webpack-plugin-template");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 exports.WEBPACK_PUB_PLUGINS = [
     ...HTML_PLUGINS
+]
+exports.WEBPACK_PRO_PLUGINS = [
+    new CleanWebpackPlugin(),
 ]
--- webpack/webpack.pro.config.js
+++ webpack/webpack.pro.config.js
@@ -2,14 +2,16 @@
  * @Description: webpack 生产配置文件
  * @Author: F-Stone
  * @Date: 2021-11-30 18:39:40
- * @LastEditTime: 2021-12-01 14:11:48
+ * @LastEditTime: 2021-12-01 15:35:40
  * @LastEditors: F-Stone
  */
 const { merge } = require("webpack-merge");
+const { WEBPACK_PRO_PLUGINS } = require("./plugins/webpack-plugin-entry");
 const PUB_CONFIG = require("./webpack.pub.config");
 
 module.exports = (env, argv) => {
     return merge(PUB_CONFIG, {
         mode: "production",
+        plugins: WEBPACK_PRO_PLUGINS
     });
 };
