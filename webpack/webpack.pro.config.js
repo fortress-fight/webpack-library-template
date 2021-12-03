752c1f62e8aa27fa547c86b5ae1be2cb0b33d479
f-stone <dreamersfly@163.com>
2021-12-03 18:11:42 +0800

    update(webpack-optimization): 添加自定义压缩配置

--- webpack/webpack.pro.config.js
+++ webpack/webpack.pro.config.js
@@ -2,10 +2,11 @@
  * @Description: webpack 生产配置文件
  * @Author: F-Stone
  * @Date: 2021-11-30 18:39:40
- * @LastEditTime: 2021-12-02 11:58:51
+ * @LastEditTime: 2021-12-03 16:06:17
  * @LastEditors: F-Stone
  */
 const { merge } = require("webpack-merge");
+const { WEBPACK_MINIMIZE } = require("./config/webpack.min");
 const { WEBPACK_PRO_PLUGINS } = require("./plugins/webpack-plugin-entry");
 const { WEBPACK_PRO_RULES } = require("./rules/webpack-rule-entry");
 const PUB_CONFIG = require("./webpack.pub.config");
@@ -15,8 +16,10 @@ module.exports = (env, argv) => {
         mode: "production",
         devtool: false,
         plugins: WEBPACK_PRO_PLUGINS,
-        module: {
-            rules: WEBPACK_PRO_RULES,
+        module: { rules: WEBPACK_PRO_RULES },
+        optimization: { 
+            ...WEBPACK_MINIMIZE,
+            removeAvailableModules: true 
         },
     });
 };
